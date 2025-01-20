2c0686b92dea50087d9a3907d5aeab55ca9d0e4b
Terry Lin <terry92516@gmail.com>
2025-01-20 19:26:29 +0800

    feat: init project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # random-station-picker
+
+隨機選擇一個人流少的捷運站出發
+
+https://www.youtube.com/watch?v=THZe4XSGl5M
+
+https://i.ytimg.com/vi/ViswL8BTHqs/hqdefault.jpg
---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const csv = require("csv-parser");
+
+function readCSVAndCalculateTraffic(filePath) {
+  const stationTraffic = {};
+
+  fs.createReadStream(filePath)
+    .pipe(csv())
+    .on("data", (row) => {
+      const station = row["捷運站別"];
+      if (!station) return;
+
+      if (!stationTraffic[station]) {
+        stationTraffic[station] = 0;
+      }
+
+      const inCount = Number.parseInt(row["進站人次"]);
+      const outCount = Number.parseInt(row["出站人次"]);
+      stationTraffic[station] += inCount + outCount;
+    })
+    .on("end", () => {
+      sortTrafficAndPickRandom(stationTraffic);
+    });
+}
+
+function sortTrafficByAsc(stations) {
+  return Object.entries(stations).sort((a, b) => a[1] - b[1]);
+}
+
+function pickRandomStationFromTop50Percent(sortedStations) {
+  const halfLength = Math.ceil(sortedStations.length / 2);
+  const top50PercentStations = sortedStations.slice(0, halfLength);
+
+  const randomIndex = Math.floor(Math.random() * top50PercentStations.length);
+  return top50PercentStations[randomIndex];
+}
+
+function sortTrafficAndPickRandom(stations) {
+  const sortedStations = sortTrafficByAsc(stations);
+
+  console.log("排序後的站點流量:");
+  console.table(sortedStations);
+
+  const randomStation = pickRandomStationFromTop50Percent(sortedStations);
+  console.log(`從人流低的前 50% 站中隨機抽取一站: ${randomStation[0]}`);
+}
+
+// 資料來源：https://data.taipei/dataset/detail?id=178ebf06-0451-4ac1-bbba-c255ca1fdac6
+readCSVAndCalculateTraffic("data.csv");
